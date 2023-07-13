e0bb09491ecb4562dda2e3c15cb372830329d9d5
TaiSimle with Zamato <tuantain2003@gmail.com>
2023-07-13 17:13:47 +0700

    Create Acount

---
+++ src/main/java/service/TaoTaiKhoanService.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package service;
+
+import com.view.model.QuanLyTaiKhoan;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.CapBac;
+import model.TaiKhoan;
+import repository.DAO_TaoTaiKhoan;
+
+/**
+ *
+ * @author LENOVO T560
+ */
+public class TaoTaiKhoanService {
+
+    private DAO_TaoTaiKhoan repositoryTK;
+
+    public TaoTaiKhoanService() {
+        try {
+            repositoryTK = new DAO_TaoTaiKhoan();
+        } catch (Exception ex) {
+            Logger.getLogger(TaoTaiKhoanService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public ArrayList<TaiKhoan> findAll() {
+//        ArrayList<TaiKhoan> listQLTK = new ArrayList<>();
+        ArrayList<TaiKhoan> listAcount = repositoryTK.findAll();
+//        for (TaiKhoan tk : listAcount) {
+//            QuanLyTaiKhoan qlTK = new QuanLyTaiKhoan(tk.getMaNV(), tk.getTenNV(), tk.getGioiTinh(), tk.getNgaySinh(), tk.getDiaChi(), tk.getSoDT(), tk.getMatKhau(), tk.getCapBac().getIdCB(), tk.getTrangThai());
+//            listQLTK.add(listAcount);
+//        }
+
+        return listAcount;
+    }
+
+    public int selectById(String id) {
+        int count = repositoryTK.selectByID(id);
+        return count;
+    }
+
+    public ArrayList<CapBac> getTenCapBac() {
+        ArrayList<CapBac> listCapBac = repositoryTK.getTenCapBac();
+        return listCapBac;
+    }
+
+    public void save(TaiKhoan acount) {
+        repositoryTK.save(acount);
+    }
+}
